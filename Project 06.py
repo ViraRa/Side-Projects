864ace5bd67e0cb8888949b591d97d9d88b23927
Viraj Rana <vir39@pitt.edu>
2020-05-12 16:53:37 -0400

    Update Project 06.py

--- Project 06.py
+++ Project 06.py
@@ -4,6 +4,8 @@ import sys
 
 class protein_charge_calc:
 
+    aa_pka = [] # made as a class variable, so I can extend this further for different projects
+
     def __init__(self, pH):
 
         protein = ""
@@ -34,8 +36,7 @@ class protein_charge_calc:
 
         # Overall complexity is O(N)
 
-    @staticmethod
-    def protein_charge(seq, pH):
+    def protein_charge(self, seq, pH):
 
         # Feel free to change the pka to anything that makes sense based on your reference
 
@@ -58,38 +59,34 @@ class protein_charge_calc:
          # 2. pH < pka -> +1
          # 3. pH == pka -> Henderson Hasselbalch  equation (also called buffer equation) 
 
-        aa_pka = []
+        for x in seq: # O(N)
 
-        for x in range(0, len(seq)): # O(N)
+            if(x == "Lys" or x == "K"):
+                self.aa_pka.append((x, 10.8, 1)) # Lys has as a NH3+ group 
+            elif(x == "Arg" or x == "R"):
+                self.aa_pka.append((x ,12.5, 1)) # Arg has as a NH3+ group 
+            elif(x == "His" or x == "H"):
+                self.aa_pka.append((x, 6, 1)) # His has as a NH+ group 
+            elif(x == "Asp" or x == "D"):
+                self.aa_pka.append((x, 3.65, 0)) # Asp has as a OH group 
+            elif(x == "Glu" or x == "E"): 
+                self.aa_pka.append((x, 4.25, 0)) # Glu has as a OH group 
+            elif(x == "Cys" or x == "C"):
+                self.aa_pka.append((x, 8.3, 0)) # Cys has a SH group
+            elif(x == "Tyr" or x == "Y"):
+                self.aa_pka.append((x, 10.9, 0)) # Tyr has a OH group
 
-            if(seq[x] == "Lys" or seq[x] == "K"):
-                aa_pka.append((seq[x], 10.8, 1)) # Lys has as a NH3+ group 
-            elif(seq[x] == "Arg" or seq[x] == "R"):
-                aa_pka.append((seq[x] ,12.5, 1)) # Arg has as a NH3+ group 
-            elif(seq[x] == "His" or seq[x] == "H"):
-                aa_pka.append((seq[x], 6, 1)) # His has as a NH+ group 
-            elif(seq[x] == "Asp" or seq[x] == "D"):
-                aa_pka.append((seq[x], 3.65, 0)) # Asp has as a OH group 
-            elif(seq[x] == "Glu" or seq[x] == "E"): 
-                aa_pka.append((seq[x], 4.25, 0)) # Glu has as a OH group 
-            elif(seq[x] == "Cys" or seq[x] == "C"):
-                aa_pka.append((seq[x], 8.3, 0)) # Cys has a SH group
-            elif(seq[x] == "Tyr" or seq[x] == "Y"):
-                aa_pka.append((seq[x], 10.9, 0)) # Tyr has a OH group
-            else:
-                aa_pka.append((seq[x], 0)) # these aa are not ionizable 
-
-        aa_pka.append(("NH3", 8.0, 1)) # for N - terminus
-        aa_pka.append(("COOH", 3.1, 0)) # for C - terminus
+        self.aa_pka.append(("NH3", 8.0, 1)) # for N - terminus
+        self.aa_pka.append(("COOH", 3.1, 0)) # for C - terminus
     
-        for x in range(0, len(aa_pka)): #O(N)
-            if(aa_pka[x][1] != 0):
-                if(pH > aa_pka[x][1]):
-                    charge_arr = np.append(charge_arr, aa_pka[x][2] - 1) # very simple
-                elif(pH < aa_pka[x][1]): # easier than the first case
-                    charge_arr = np.append(charge_arr, aa_pka[x][2])
-                elif(pH == aa_pka[x][1]): # a little difficult since have to use buffer equation
-                    ratio_base_to_acid = 10 ** (pH - aa_pka[x][1])
+        for x in self.aa_pka: #O(N)
+            if(x[1] != 0):
+                if(pH > x[1]):
+                    charge_arr = np.append(charge_arr, x[2] - 1) # very simple
+                elif(pH < x[1]): # easier than the first case
+                    charge_arr = np.append(charge_arr, x[2])
+                elif(pH == x[1]): # a little difficult since have to use buffer equation
+                    ratio_base_to_acid = 10 ** (pH - x[1])
                     ratio_base_to_acid_simp = Fraction(ratio_base_to_acid).limit_denominator()
             
                     if(ratio_base_to_acid_simp > 1):
@@ -99,5 +96,6 @@ class protein_charge_calc:
                     elif(ratio_base_to_acid_simp == 1):
                         charge_arr = np.append(charge_arr, -0.5)
         return charge_arr
+    
 
 obj = protein_charge_calc(2.34) # change pH value here
